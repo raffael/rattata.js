ebe3f008cebc2930c13385afa6c1e7af01fafc20
Raffael <raffael@raffael.me>
2012-02-12 18:29:49 +0100

    .

--- plugins/rattata.tuioJSONfy/rattata.tuioJSONfy.js
+++ plugins/rattata.tuioJSONfy/rattata.tuioJSONfy.js
@@ -3,67 +3,15 @@
 	var core	= {
 	
 		loadTuioJSON: function(){
-			var tuioParserComplete	= false,
-				twFixorComplete		= false;
-			
-			
-			/**
-			 * as soon as both scripts have been loaded, create the parser and the WebSocket connection
-			 */
-			function injectingTuioComplete(){
-				if (tuioParserComplete && twFixorComplete) {
-					window.parserProxy	= new TWFixor({
-						tuioJSONParser:	new tuioJSONParser({
-							// parsing options
-						})
-					});
-					
-					// initialize a WebSocket Object
-					socket = new WebSocket('ws://127.0.0.1:8787/jWebSocket/jWebSocket');
-					
-					// define Callback handler for opOpen event
-					socket.onopen = function(){
-						var registerMessage = '{"ns":"de.dfki.touchandwrite.streaming","type":"register","stream":"touchandwriteevents"}';
-						socket.send(registerMessage);
-						console.log("tuioJSON is ready");
-					}
-					
-					// define Callback handler for onMessage event
-					socket.onmessage = function(msg){
-						// extract JSON data from message
-						var data = JSON.parse(msg.data);
-						// and pass it to the TuioJSON parser
-						window.parserProxy.fix(data);
-					}
-				}
-			}
+			var script		= document.createElement('script');
+			script.src		='https://raw.github.com/raffael-me/tuioJSON-Parser/master/enableTuioJSON.tw.git.js';
+			script.type		='text/javascript';
+			script.async		= true;
 			
 			/**
-			 * prepare script injecting and define onload event handlers
+			 * append the script
 			 */
-			var scriptTuioParser	= document.createElement('script');
-			scriptTuioParser.src	='https://raw.github.com/raffael-me/tuioJSON-Parser/master/lib/tuioJSONParser.js';
-			scriptTuioParser.type	='text/javascript';
-			scriptTuioParser.async	= true;
-			scriptTuioParser.onload	= function(){
-				tuioParserComplete	= true;
-				injectingTuioComplete();
-			}
-			
-			var scriptTWFixor		= document.createElement('script');
-			scriptTWFixor.src		='https://raw.github.com/raffael-me/tuioJSON-Parser/master/TWFixor.j';
-			scriptTWFixor.type		='text/javascript';
-			scriptTWFixor.async		= true;
-			scriptTWFixor.onload	= function(){
-				twFixorComplete		= true;
-				injectingTuioComplete();
-			}
-			
-			/**
-			 * append the two scripts
-			 */
-			document.head.appendChild(scriptTuioParser);
-			document.head.appendChild(scriptTWFixor);
+			document.head.appendChild(script);
 		},
 		
 		/**
